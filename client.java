b9e090e41f65a582333aef2a6020e709b9cc921e
csdemi <cdemianew@gmail.com>
2016-05-11 15:14:28 -0700

    Add files via upload

---
+++ client.java
@@ -0,0 +1,10 @@
+import travelpackage.utilities;
+/**
+ * Created by Cody on 2/22/2016.
+ */
+class client
+{
+    public static void main(String []args) {
+        utilities.executeOrder66();
+    }
+}
